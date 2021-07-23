255966fb2b07e0109c0c16a674a50992d41cd90f
fourteen-0-0-eyes <87821092+fourteen-0-0-eyes@users.noreply.github.com>
2021-07-23 11:31:30 +0300

    Delete Task3 directory

--- Task3/markup.py
+++
@@ -1,6 +0,0 @@
-from aiogram import types
-
-# --- Play Button ---
-start_markup = types.InlineKeyboardMarkup()
-start_button = types.InlineKeyboardButton('Начать играть', callback_data='play')
-start_markup.add(start_button)
